c4cfc1f31e804160154685f0076ca0ff56f3ae83
Jens Bouma <account@jensbouma.nl>
2023-06-09 16:22:47 +0200

    🔥 chore(docker-image.yml): remove docker-image.yml workflow file 🚀 feat(docker-publish.yml): add support for multi-architecture builds and remove cosign installation step 🐛 fix(app.py): add support for WHISPER_MODEL environment variable to be able to choose the whisper model to use

--- whisper-api/app.py
+++ whisper-api/app.py
@@ -2,20 +2,22 @@ from flask import Flask, abort, request
 from tempfile import NamedTemporaryFile
 import whisper
 import torch
+import os
+
+if "WHISPER_MODEL" not in os.environ:
+	whisper_model = "small"
+else:
+	whisper_model = os.environ["WHISPER_MODEL"]
 
 torch.cuda.is_available()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-model = whisper.load_model("large", download_root="/var/models", device=DEVICE)
+model = whisper.load_model(whisper_model, download_root="/var/models", device=DEVICE)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Whisper API"
-
-@app.route('/whisper', methods=['POST'])
+@app.route('/', methods=['POST'])
 def handler():
 	if not request.files:
 		abort(400, "No file provided")
